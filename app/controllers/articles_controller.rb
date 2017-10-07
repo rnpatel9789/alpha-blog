ff3dcb51a28a74b7d89926bc676c66487dec7f5c
Ravi Narayan Patel <rnpatel9789@gmail.com>
2017-10-07 09:11:01 +0000

    Create new template action and route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController 
+  def new 
+    @article = Article.new
+  end 
+  def create 
+    # render plain: params[:article].inspect
+    @article = Article.new(articles_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+  private 
+  def articles_params 
+    params.require(:article).permit(:title,:description)
+  end
+end
